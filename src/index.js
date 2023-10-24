88d1e64e52fc5c6aea921eae53a470c0295b2ad8
Ezequiel Amin <ezequielamin@outlook.com>
2023-10-24 15:50:33 -0300

    configuramos express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hola mundo desde el backend!!!!!")
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+// 1. Iniciar la aplicación
+const app = express();
+
+// 2. Configuraciones generales
+const PORT = process.env.PORT || 5000;
+
+// 3. Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// 4. Rutas
+
+// 5. Iniciar el loop del servidor
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
